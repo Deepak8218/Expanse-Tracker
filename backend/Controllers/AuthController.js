c253f7de7d3da5f3a7c6b757f148711988f0ca36
Deepak8218 <128400567+Deepak8218@users.noreply.github.com>
2024-11-22 22:28:01 +0530

    Update AuthController.js

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -24,7 +24,7 @@ const signup = async (req, res) => {
             .json({
                 message: "Internal server errror",
                 success: false,
-                err
+                error: err
             })
     }
 }
