a8f51f32234150745ec868e4c9ffa082b25ff0f4
Deepak8218 <128400567+Deepak8218@users.noreply.github.com>
2024-11-22 12:19:27 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -38,7 +38,7 @@ function Home() {
 
     const deleteExpens = async (id) => {
         try {
-            const url = `${APIUrl}/expenses/${id}`;
+            const url = `https://expanse-tracker-phi.vercel.app/expenses/${id}`;
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
